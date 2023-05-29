12f93ee4703a8de87c4919c348a2fc537492ee5e
Jhulia <jhulias195@gmail.com>
2023-05-28 23:45:04 -0300

    adicionando section dos personagens

---
+++ site/public/js/char-slider.js
@@ -0,0 +1,20 @@
+var raiden = document.getElementById("raiden");
+var zhongli = document.getElementById("zhongli");
+var venti = document.getElementById("venti");
+var nahida = document.getElementById("nahida");
+
+raiden.addEventListener("click", function() {
+    raiden.classList.toggle("flip");
+});
+
+zhongli.addEventListener("click", function() {
+    zhongli.classList.toggle("flip");
+});
+
+venti.addEventListener("click", function() {
+    venti.classList.toggle("flip");
+});
+
+nahida.addEventListener("click", function() {
+    nahida.classList.toggle("flip");
+});
