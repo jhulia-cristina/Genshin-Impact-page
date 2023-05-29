e9977dd5c85fbc7859ac18bb3df62b1af8125da0
Jhulia <jhulia.silva@sptech.school>
2023-05-29 16:06:49 -0300

    Adicionando função de login e tela de dashboard

---
+++ site/public/js/signin.js
@@ -0,0 +1,64 @@
+function entrar() {
+    // aguardar();
+
+    var emailVar = input_email.value;
+    var senhaVar = input_senha.value;
+
+    if (emailVar == "" || senhaVar == "") {
+        // cardErro.style.display = "block"
+        swal ("Ops" , "Preencha os campos para fazer o login",  "error");
+        // finalizarAguardar();
+        return false;
+    }
+    // else {
+    //     setInterval('erro', 5000)
+    // }
+
+    console.log("FORM LOGIN: ", emailVar);
+    console.log("FORM SENHA: ", senhaVar);
+
+    fetch("/usuarios/autenticar", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            emailServer: emailVar,
+            senhaServer: senhaVar
+        })
+    }).then(function (resposta) {
+        console.log("ESTOU NO THEN DO entrar()!")
+
+        if (resposta.ok) {
+            console.log(resposta);
+
+            resposta.json().then(json => {
+                console.log(json);
+                console.log(JSON.stringify(json));
+
+                sessionStorage.EMAIL_USUARIO = json.email;
+                sessionStorage.NOME_USUARIO = json.nome;
+                sessionStorage.ID_USUARIO = json.id;
+
+                setTimeout(function () {
+                    window.location = "screens/dashboard.html";
+                }, 1000); // apenas para exibir o loading
+
+            });
+
+        } else {
+
+            swal ("Ops",  "Email e/ou senha incorreto(s)" ,  "error");
+
+            resposta.text().then(texto => {
+                console.error(texto);
+                // finalizarAguardar(texto);
+            });
+        }
+
+    }).catch(function (erro) {
+        console.log(erro);
+    })
+
+    return false;
+    }
